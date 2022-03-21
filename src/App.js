0abacadac69ed3e35c543dbbac90156f3a9fd2fb
Drew-bozarth <dbozarth@chapman.edu>
2022-03-20 22:33:16 -0700

    sudoku solver is working. need to make a check puzzle.

--- src/App.js
+++ src/App.js
@@ -1,47 +1,104 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React, { Component } from "react";
+import generator from "sudoku";
+import produce from "immer";
+import SudokuBoard from "./componenets/SudokuBoard";
+import "./App.css";
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
+/*
+  Generates a sudoku with the structure
+
+  {rows: [{index: 0, cols: [{row: 0, col: 0, value: 1, readonly: true}, ...]}, ...]}
+
+*/
+function generateSudoku() {
+  const raw = generator.makepuzzle();
+  const rawSolution = generator.solvepuzzle(raw)
+  
+  const formatted = raw.map(e => (e === null ? null : e + 1));
+  const formattedSolution = rawSolution.map(e => e + 1);
+
+  const result = {
+    rows: [], 
+    solution: formattedSolution,
+    startTime: new Date(),
+    solvedTime: null
+  };
+
+  for (let i=0; i<9; i++) {
+    const row = {cols: [], index: i}
+    for (let j=0; j<9; j++) {
+      const value = raw[i*9+j]
+      const col = {
+        row: i,
+        col: j,
+        value: value,
+        readonly: value !== null
+      };
+      row.cols.push(col);
+    }
+    result.rows.push(row)
   }
 
-  handleClick = api => e => {
-    e.preventDefault()
+  return result;
+}
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
+function checkSolution(sudoku) {
+  const candidate = sudoku.rows
+    .map(row => row.cols.map(col => col.value))
+    .flat();
+
+  for (let i=0; i<candidate.length; i++) {
+    if (candidate[i] === null || candidate[i] !== sudoku.solution[i]) {
+      return false;
+    }
   }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
+  return true;
 }
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = produce({}, () => ({
+      sudoku: generateSudoku()
+    }));
+  }
+
+  handleChange = e => {
+    this.setState(
+      produce(state => {
+        state.sudoku.rows[e.row].cols[e.col].value = e.value;
+        if (!state.sudoku.solvedTime) {
+          const solved = checkSolution(state.sudoku)
+          if (solved) {
+            state.sudoku.solveTime = new Date();
+          }
+        }
+      })
+    );
+  };
+
+  solveSudoku = e => {
+    this.setState(
+      produce(state => {
+        state.sudoku.rows.forEach(row => 
+          row.cols.forEach(col => {
+            col.value = state.sudoku.solution[col.row*9+col.col];
+          })
+        );
+      })
+    );
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
+          <h1>Sudoku</h1>
+          {/* <h4>created by Drew Bozarth</h4> */}
         </header>
+        <SudokuBoard sudoku={this.state.sudoku} onChange={this.handleChange} />
+
+        <button onClick={this.solveSudoku}>Solve</button>
       </div>
     )
   }
