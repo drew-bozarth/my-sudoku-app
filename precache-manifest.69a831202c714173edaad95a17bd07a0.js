b653baf7de43070b62ea202377ac241525a9598d
Drew-bozarth <dbozarth@chapman.edu>
2022-04-01 23:36:17 -0700

    Updates

---
+++ precache-manifest.69a831202c714173edaad95a17bd07a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df75561d3cbc7eb3e08fc634f0109b12",
+    "url": "/my-sudoku-app/index.html"
+  },
+  {
+    "revision": "f75de5b1fbd28d02ed83",
+    "url": "/my-sudoku-app/static/css/main.a42f0d83.chunk.css"
+  },
+  {
+    "revision": "8943ebef00fe6c2bb2af",
+    "url": "/my-sudoku-app/static/js/2.46b9030e.chunk.js"
+  },
+  {
+    "revision": "f75de5b1fbd28d02ed83",
+    "url": "/my-sudoku-app/static/js/main.afff684e.chunk.js"
+  },
+  {
+    "revision": "bcc096124ac6b43827f9",
+    "url": "/my-sudoku-app/static/js/runtime~main.69a34a82.js"
+  }
+]);
